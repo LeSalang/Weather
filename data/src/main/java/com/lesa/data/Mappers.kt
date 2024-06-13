11061b1bfb3c162dd8997cc8767ac4b2547e606a
LeSa <le.salang@yandex.ru>
2024-06-14 00:35:51 +0400

    Clean weather mapper

--- data/src/main/java/com/lesa/data/Mappers.kt
+++ data/src/main/java/com/lesa/data/Mappers.kt
@@ -2,6 +2,9 @@ package com.lesa.data
 
 import com.lesa.api.models.CurrentWeatherResponseDto
 import com.lesa.data.models.CurrentWeather
+import java.math.RoundingMode
+import kotlin.math.roundToInt
+import kotlin.math.roundToLong
 
 const val FAHRENHEIT_TO_CELSIUS_SCALE = 5.0 / 9.0
 const val FAHRENHEIT_TO_CELSIUS_OFFSET = 32
@@ -9,9 +12,10 @@ const val INCHES_TO_MM_SCALE = 25.4
 
 internal fun CurrentWeatherResponseDto.toCurrentWeather(): CurrentWeather {
     val tempC = currentWeather.tempC ?: currentWeather.tempF.let {
-        (it - FAHRENHEIT_TO_CELSIUS_OFFSET) * FAHRENHEIT_TO_CELSIUS_SCALE
-    } // Provide Celsius or Convert Fahrenheit to Celsius
-    val pressureMmHg = currentWeather.pressureIn * INCHES_TO_MM_SCALE // Convert inches to mm Hg
+        ((it - FAHRENHEIT_TO_CELSIUS_OFFSET) * FAHRENHEIT_TO_CELSIUS_SCALE * 10).roundToInt() / 10.0
+    }
+
+    val pressureMmHg = (currentWeather.pressureIn * INCHES_TO_MM_SCALE * 10).roundToInt() / 10.0
 
     return CurrentWeather(
         name = location.name,
